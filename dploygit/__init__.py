20495b66492e355bd14717f95e102961eaf97df5
Reuven V. Gonzales <reuven@tobetter.us>
2012-07-27 13:18:29 -0700

    Starting development

---
+++ dploygit/__init__.py
@@ -0,0 +1,10 @@
+import sys
+from .hooks import PreReceiveHook
+
+
+def pre_receive():
+    """Main entry point for dploygit pre-receive hook.
+
+    Just run this inside a hook.
+    """
+    PreReceiveHook.run(sys.stdin)

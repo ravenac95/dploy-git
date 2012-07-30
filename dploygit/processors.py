96b32a534bad1c5d81babafc81a4b03245a0fdd4
Reuven V. Gonzales <reuven@tobetter.us>
2012-07-30 11:13:09 -0700

    Adding GitUpdate

--- dploygit/processors.py
+++ dploygit/processors.py
@@ -1,3 +1,7 @@
+class GitUpdate(object):
+    pass
+
+
 class PreReceiveProcessor(object):
     def __init__(self, build_queue_client, broadcast_listener, git_repository,
             output):
@@ -8,4 +12,19 @@ class PreReceiveProcessor(object):
 
     def process(self, line):
         output = self._output
-        output.write(line)
+
+        update = GitUpdate.from_line(line)
+        branch = update.branch
+        if branch == 'master':
+            output.line('Receiving new code from master branch')
+            # Export the file to a temporary file
+            update_file = update.export_to_file()
+            # Queue the DeployRequest
+            # Should receive a listening channel in the response
+            response = self._build_queue_client.send_deploy_request(
+                    self._git_repository.name, update_file)
+            # Wait and listen to the broadcaster using the received
+            # listening channel
+            self._broadcast_listener.listen_from_response(response)
+        else:
+            output.line('Ignoring branch "%s"' % branch)
